52ad5205256710ebb6843d365639221bfefab7ff
gabby <gabby.davison03@gmail.com>
2026-04-21 09:40:42 -0400

    Revert "insurance percentage page works now"
    This reverts commit be682f32ef307a0eea3c5a0a83ef92492685bb03.

--- WCHDApp/admin.py
+++ WCHDApp/admin.py
@@ -4,12 +4,11 @@ from .models import Fund, Line, Dept, Item, Employee, People, ActivityList, Payr
 
 class PeopleAdmin(admin.ModelAdmin):
     search_fields = ['name']
-#creating = self._state.adding
-#self.line = line
+creating = self._state.adding
+self.line = line
 
 class ExpenseAdmin(admin.ModelAdmin):
     autocomplete_fields = ['people']
-    """
     if creating:
             self.line = self.item.line
             print(f"Full ID: {self.expenseFullID}")
@@ -19,7 +18,6 @@ class ExpenseAdmin(admin.ModelAdmin):
                 date = datetime.now().date()
                 fullID = f"{self.employee.employee_id}-{self.ActivityList.ActivityList_id}-{date.isoformat()}-{timeNow}"
                 self.expenseFullID = fullID
-    """
 
 class GrantAdmin(admin.ModelAdmin):
     list_display = ("grant_id", "grant_name")
